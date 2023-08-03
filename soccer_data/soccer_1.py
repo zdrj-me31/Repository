7a9a53f03ec4a98315e1b6af3c547f0e578f1278
JanZdr <61503850+zdrj-me31@users.noreply.github.com>
2023-08-03 15:20:36 +0200

    change in 1st line

--- soccer_data/soccer_1.py
+++ soccer_data/soccer_1.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 import numpy as np
 
-soccer_data = pd.read_csv('soccer_data.csv')
+soccer_data = pd.read_csv("soccer_data.csv")
 
 # print head
 print(soccer_data.head())
@@ -17,32 +19,31 @@ print(soccer_data.index)
 
 
 # values sorted by number of yellow cards recieved by home team
-yellow_card_home = soccer_data.sort_values('HY', ascending=False)
+yellow_card_home = soccer_data.sort_values("HY", ascending=False)
 print(yellow_card_home)
 
 
 # win home and HST is more than 4
 
-win_home = soccer_data['FTR'] == 'H'
-more_than_four = soccer_data['HST'] > 6
+win_home = soccer_data["FTR"] == "H"
+more_than_four = soccer_data["HST"] > 6
 
 home_win_more_thanfourshots = soccer_data[win_home & more_than_four]
-print(home_win_more_thanfourshots['HomeTeam'])  # leicester, chelsea, crystalP
+print(home_win_more_thanfourshots["HomeTeam"])  # leicester, chelsea, crystalP
 
 # sort by number of yellow cards from home team and desc yellow by away
 
-yellow_cards_home_away = soccer_data.sort_values(
-    ['HY', 'AY'], ascending=[False, True])
+yellow_cards_home_away = soccer_data.sort_values(["HY", "AY"], ascending=[False, True])
 print(yellow_cards_home_away.head())
 
-home_manu = soccer_data[soccer_data['HomeTeam'] == 'Man United']
-away_manu = soccer_data[soccer_data['AwayTeam'] == 'Man United']
+home_manu = soccer_data[soccer_data["HomeTeam"] == "Man United"]
+away_manu = soccer_data[soccer_data["AwayTeam"] == "Man United"]
 
 print(home_manu.describe())
 
 # win or draw when manchester united was a host
 
-win_draw_home_manu = home_manu[home_manu['FTR'].isin(['H', 'D'])]
+win_draw_home_manu = home_manu[home_manu["FTR"].isin(["H", "D"])]
 print(win_draw_home_manu)
 
 
@@ -50,18 +51,21 @@ def iqr(column):
     return column.quantile(0.75) - column.quantile(0.25)
 
 
-print(soccer_data[['HS', 'HTHG']].agg(iqr))
+print(soccer_data[["HS", "HTHG"]].agg(iqr))
 
 # value counts for wins draws loses
-results_home_manu_values = home_manu['HTR'].value_counts(normalize=True)
+results_home_manu_values = home_manu["HTR"].value_counts(normalize=True)
 print(results_home_manu_values)
 
 
 # pivot table with mediana of FTHG, HS, HST, columns as FTR, index as home teams
-our_home_table = soccer_data.pivot_table(values=['FTHG', 'HST', 'HS'],
-                                         index='HomeTeam',
-                                         columns='FTR', fill_value=0,
-                                         aggfunc=np.median)
+our_home_table = soccer_data.pivot_table(
+    values=["FTHG", "HST", "HS"],
+    index="HomeTeam",
+    columns="FTR",
+    fill_value=0,
+    aggfunc=np.median,
+)
 
 
 # homeTeam as indexes
@@ -72,4 +76,4 @@ homeTeam_ind = soccer_data.set_index("HomeTeam")
 my_example = homeTeam_ind.loc[["Man United", "Arsenal", "Chelsea"]]
 
 # subset by index
-by_index = soccer_data[soccer_data['HomeTeam'].isin(["Man United", "Arsenal"])]
+by_index = soccer_data[soccer_data["HomeTeam"].isin(["Man United", "Arsenal"])]
